190dd13d37231c9101d4f6562fa189bb0cd5520f
Cheedge <cheedgelee@gmail.com>
2022-03-01 22:48:47 +0100

    some collection exercises

---
+++ CollectionsEx/src/com/lqz/list/CollectionsListEx.java
@@ -0,0 +1,13 @@
+package com.lqz.list;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class CollectionsListEx {
+	@Test
+	public void test1() {
+//		List list = new List();
+		
+	}
+}
